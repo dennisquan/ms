cbf9277193783087e1c92379c4be1431cb55b69b
Dennis Quan <dquan@alum.mit.edu>
2026-01-22 08:48:37 -0500

    bug fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,11 +8,6 @@ const router = createRouter({
       name: 'home',
       component: () => import('../views/Home.vue')
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/About.vue')
-    }
   ]
 })
 
